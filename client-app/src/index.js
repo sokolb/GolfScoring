180ad61292da81bed941c4eaf3a38fc93e60dd94
Brian Sokoloski <brian.sokoloski@gmail.com>
2025-11-29 19:33:17 -0600

    Phase2: buttons and typography

--- client-app/src/index.js
+++ client-app/src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/theme.css";
+import "./styles/typography.css";
+import "./styles/buttons.css";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import configureStore from "./Store/configureStore";
