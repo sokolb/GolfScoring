fe102e1578f396c5402a6f7fa220912ceea04249
Brian Sokoloski <brian.sokoloski@gmail.com>
2025-11-29 19:40:37 -0600

    more UI updates

--- client-app/src/index.js
+++ client-app/src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./styles/theme.css";
 import "./styles/typography.css";
 import "./styles/buttons.css";
+import "./styles/forms.css";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import configureStore from "./Store/configureStore";
